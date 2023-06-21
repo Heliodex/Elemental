cd95f507ba80ff5b4b58571193e60771f4855816
Lewin Kelly <heli@odex.cf>
2023-06-21 23:45:01 +0100

    Import types correctly for trpc

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -1,5 +1,5 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client"
-import type {AppRouter} from "/server"
+import type { AppRouter } from "../../server/index"
 
 export default createTRPCProxyClient<AppRouter>({
 	links: [
